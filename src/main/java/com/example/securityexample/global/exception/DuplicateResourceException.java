a447d69b77e001600ad280dd283c9f9066277b3c
JinHoooooou <jinho4744@naver.com>
2024-07-02 23:26:17 +0900

    사용하지 않는 메서드 삭제

--- src/main/java/com/example/securityexample/global/exception/DuplicateResourceException.java
+++ src/main/java/com/example/securityexample/global/exception/DuplicateResourceException.java
@@ -6,15 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.CONFLICT)
 public class DuplicateResourceException extends RuntimeException {
 
-  public DuplicateResourceException() {
-    super();
-  }
-
   public DuplicateResourceException(String message) {
     super(message);
-  }
-
-  public DuplicateResourceException(String message, Throwable cause) {
-    super(message, cause);
   }
 }
