f7589ee3bd03a1dc6bedcf276156a9fa347b22b3
JinHoooooou <jinho4744@naver.com>
2024-07-08 17:01:32 +0900

    Spring Security Config 추가

---
+++ src/main/java/com/example/securityexample/global/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.example.securityexample.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity.formLogin(AbstractHttpConfigurer::disable)
+        .csrf(AbstractHttpConfigurer::disable)
+        .headers(frame -> frame.frameOptions(FrameOptionsConfig::sameOrigin));
+
+    httpSecurity.authorizeHttpRequests(requests -> requests
+        .requestMatchers(HttpMethod.POST, "/api/v1/login", "/api/v1/signup").permitAll()
+        .requestMatchers("/h2-console/**").permitAll()
+        .requestMatchers("/").permitAll()
+    );
+
+    return httpSecurity.build();
+  }
+}
